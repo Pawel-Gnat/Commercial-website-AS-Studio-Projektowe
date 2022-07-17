d6ae88b6bf6e108a27329eb40fd6bd36481cb7bb
Paweł <pawel_gnat@o2.pl>
2022-07-17 20:35:09 +0200

    created starting files and added few lines of code

---
+++ README.md
@@ -0,0 +1,3 @@
+In progress..
+
+Commercial website for landscape and interior architecture company
---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+const menuBtn = document.querySelector('.menu__btn')
+const animateBtn = document.querySelector('.active')
+const displayLinks = document.querySelector('.display')
+const linksBox = document.querySelector('.menu__box')
+const body = document.querySelector('body')
+const disableScroll = document.querySelector('.overflow')
+const menuLinks = document.querySelectorAll('.menu__box--links a')
+const buttonLines = Array.from(document.querySelectorAll('.menu__btn--line')) // an array with my button menu lines
+
+function toggleActive() {
+	// function animate my hamburger icon, show mobile menu and prevent vertical scroll
+	buttonLines.forEach(
+		// function triggers my array elements (hamburger menu lines) and animate them
+		line => {
+			line.classList.toggle('active')
+		}
+	)
+	linksBox.classList.toggle('display')
+	body.classList.toggle('overflow')
+}
+
+menuLinks.forEach(links => {
+	// loop function that closes my mobile menu view while I select one of website links
+	links.addEventListener('click', () => {
+		buttonLines.forEach(line => {
+			line.classList.toggle('active')
+		})
+		linksBox.classList.toggle('display')
+		body.classList.toggle('overflow')
+	})
+})
+
+menuBtn.addEventListener('click', toggleActive)
+
+
